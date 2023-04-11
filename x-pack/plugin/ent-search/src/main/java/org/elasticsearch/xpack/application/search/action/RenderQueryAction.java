6d57af94ef2fc9a1bbdb710bc68d21f2843c8818
Kathleen DeRusso <kathleen.derusso@elastic.co>
2023-04-11 16:10:24 -0400

    fix compile error

--- x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/search/action/RenderQueryAction.java
+++ x-pack/plugin/ent-search/src/main/java/org/elasticsearch/xpack/application/search/action/RenderQueryAction.java
@@ -43,7 +43,7 @@ public class RenderQueryAction extends ActionType<RenderQueryAction.Response> {
         private final Map<String, Object> renderedTemplateParams;
 
         private static final ConstructingObjectParser<Request, String> PARSER = new ConstructingObjectParser<>(
-            QUERY_PARAMS_FIELD,
+            "params",
             false,
             (params, searchAppName) -> {
                 @SuppressWarnings("unchecked")
